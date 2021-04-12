e065140d77fc3c65dda41af893d9f4fe8cac414c
Ahmad Ihsan <ihsanashi@gmail.com>
2021-04-12 09:35:28 +0800

    wrap entire app with context

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import '../styles/globals.css';
 import { ThemeProvider } from 'next-themes';
+import { AppWrapper } from '../src/context/state';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute='class'>
-      <Component {...pageProps} />
+      <AppWrapper>
+        <Component {...pageProps} />
+      </AppWrapper>
     </ThemeProvider>
   );
 }
