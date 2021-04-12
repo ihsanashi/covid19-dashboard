5e344e8c41106f7b3c4e64c4a33479fe33fd7143
Ahmad Ihsan <ihsanashi@gmail.com>
2021-04-12 09:36:27 +0800

    format table cell, move table data fetch call to useEffect hook

--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,106 @@
 import Head from 'next/head';
-import Header from '../components/Header';
-import MapInfo from '../components/MapInfo';
-import Table from '../components/Table';
-import { useMemo, useState, useEffect } from 'react';
+import Header from '../src/components/Header';
+import MapInfo from '../src/components/MapInfo';
+import Table from '../src/components/Table';
+import { useMemo, useState, useContext, useEffect } from 'react';
+import { AppContext } from '../src/context/state';
+import axios from 'axios';
 
-export default function Home({ overviewData, tableData }) {
-  console.log(tableData);
+export default function Home({ overviewData }) {
+  const [state, setState] = useContext(AppContext);
+  const { timeFrame } = state;
+  const [tableData, setTableData] = useState([]);
+  const baseUrl = 'https://disease.sh/v3/covid-19';
+
+  const getTableData = async () => {
+    try {
+      let tableRes;
+      if (timeFrame === 'today') {
+        tableRes = await axios.get(`${baseUrl}/countries`);
+      } else if (timeFrame === 'yesterday') {
+        tableRes = await axios.get(`${baseUrl}/countries?yesterday=true`);
+      } else {
+        tableRes = await axios.get(`${baseUrl}/countries?twoDaysAgo=true`);
+      }
+      const tableData = await tableRes.data;
+      console.log(tableData);
+      setTableData(tableData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getTableData();
+  }, [timeFrame]);
 
   const columns = useMemo(() => [
     {
-      Header: 'Country or region',
+      Header: 'Country',
       accessor: 'country',
     },
     {
       Header: 'Total cases',
       accessor: 'cases',
+      Cell: ({ cell: { value } }) => {
+        return <>{value.toLocaleString()}</>;
+      },
     },
     {
       Header: 'New cases',
       accessor: 'todayCases',
+      Cell: ({ cell: { value } }) => {
+        return <>{value.toLocaleString()}</>;
+      },
     },
     {
       Header: 'Total deaths',
       accessor: 'deaths',
+      Cell: ({ cell: { value } }) => {
+        return <>{value.toLocaleString()}</>;
+      },
     },
     {
       Header: 'New deaths',
       accessor: 'todayDeaths',
+      Cell: ({ cell: { value } }) => {
+        return <>{value.toLocaleString()}</>;
+      },
     },
     {
       Header: 'Total recovered',
       accessor: 'recovered',
+      Cell: ({ cell: { value } }) => {
+        return <>{value.toLocaleString()}</>;
+      },
     },
     {
       Header: 'New recoveries',
       accessor: 'todayRecovered',
+      Cell: ({ cell: { value } }) => {
+        return <>{value.toLocaleString()}</>;
+      },
     },
     {
       Header: 'Total tests',
       accessor: 'tests',
+      Cell: ({ cell: { value } }) => {
+        return <>{value.toLocaleString()}</>;
+      },
     },
     {
       Header: 'Tests/1m pop',
       accessor: 'testsPerOneMillion',
+      Cell: ({ cell: { value } }) => {
+        return <>{value.toLocaleString()}</>;
+      },
     },
     {
       Header: 'Population',
       accessor: 'population',
+      Cell: ({ cell: { value } }) => {
+        return <>{value.toLocaleString()}</>;
+      },
     },
   ]);
 
@@ -59,28 +113,22 @@ export default function Home({ overviewData, tableData }) {
 
       <main>
         <Header data={overviewData} />
-        <MapInfo />
+        {/* <MapInfo /> */}
         <Table columns={columns} data={tableData} />
-
-        {/* <footer>
-          <p>Made by Ahmad Ihsan</p>
-        </footer> */}
       </main>
     </div>
   );
 }
 
 export async function getStaticProps() {
-  const overviewRes = await fetch('https://disease.sh/v3/covid-19/all');
-  const overviewData = await overviewRes.json();
+  const baseUrl = 'https://disease.sh/v3/covid-19';
 
-  const tableRes = await fetch('https://disease.sh/v3/covid-19/countries');
-  const tableData = await tableRes.json();
+  const overviewRes = await fetch(`${baseUrl}/all`);
+  const overviewData = await overviewRes.json();
 
   return {
     props: {
       overviewData,
-      tableData,
     },
   };
 }
