b769f93e2d27e9a751c1f3b2ffaf4b09c82a42dd
Ahmad Ihsan <ihsanashi@gmail.com>
2021-04-12 11:15:39 +0800

    add responsive font size for summary card title

--- src/components/SummaryCard.js
+++ src/components/SummaryCard.js
@@ -1,7 +1,7 @@
 const SummaryCard = ({ title, subtitle }) => {
   return (
     <div className='flex flex-col justify-start py-5 px-4 bg-white rounded-lg shadow-lg'>
-      <h4 className='font-medium text-2xl text-gray-900'>
+      <h4 className='font-medium text-xl md:text-2xl text-gray-900'>
         {title.toLocaleString()}
       </h4>
       <p className='font-normal text-lg text-gray-500'>{subtitle}</p>
